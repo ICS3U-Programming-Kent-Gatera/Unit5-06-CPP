66c7d7d9a1348052ccb3d19df79b837bf9db0d49
Kent Gatera <kent.gatera@stu.ocsb.ca>
2022-12-12 19:22:27 +0000

    rounding program

---
+++ rounding.cpp
@@ -0,0 +1,41 @@
+// Copyright (c) 2022 Kent Gatera All rights reserved.
+//
+// Created by: Kent Gatera
+// Created on: Dec. 10, 2022
+// This program rounds numbers to a chosen point.
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <iomanip>
+
+// Defining the rounding function.
+void roundDec(float &decimalNum, float roundNum) {
+    decimalNum *= pow(10, roundNum);
+    decimalNum += 0.5;
+    decimalNum = static_cast<int>(decimalNum);
+    decimalNum = decimalNum / pow(10, roundNum);
+}
+
+int main() {
+    float userDecimal;
+    int userRound;
+    // Error checking
+    try {
+        // Getting input.
+        std::cout << "What decimal do you want to round?: ";
+        std::cin >> userDecimal;
+        std::cout << "What decimal place do you want to round it to?: ";
+        std::cin >> userRound;
+        roundDec(userDecimal, userRound);
+
+        // Rounding visually the decimal so even
+        // if there were no decimal places, you can see how many.
+        std::cout << std::fixed << std::setprecision(userRound) <<
+        userDecimal <<
+        " rounded was to " <<
+        userRound << " decimal places.";
+    } catch(std::invalid_argument) {
+        std::cout << "Invalid input.";
+    }
+}
